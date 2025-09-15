abf3aabd371478868d30058a2838885b45d55a75
Harrizon Soler <harrizon.soler@usantoto.edu.co>
2025-09-15 06:53:34 -0500

    fix(react): added missing protocol

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -37,8 +37,8 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <h1>Hey Team! 👋</h1>
-      <CurrentTime api="localhost:8080"/>
-      <CurrentTime api="localhost:3000"/>
+      <CurrentTime api="http://localhost:8080"/>
+      <CurrentTime api="http://localhost:3000"/>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
